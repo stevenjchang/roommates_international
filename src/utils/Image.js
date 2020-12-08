45114aa5e09c25e41bbb2669a38132c708ede6cc
stevenjchang <stevenjchang@gmail.com>
2020-12-07 18:27:51 -0800

    add listing images

---
+++ src/utils/Image.js
@@ -0,0 +1,13 @@
+class Image {
+  static getImageUrl(id) {
+    if (typeof id !== "number") {
+      console.log("Error: id is not a number (utils/Image) ==>");
+      return "";
+    }
+
+    const formattedId = id.toString().length > 1 ? id : "0" + id;
+    return `/img/listing/${formattedId}.jpg`;
+  }
+}
+
+export default Image;
