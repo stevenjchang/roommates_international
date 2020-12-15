cc368e3a640f2de1868e5a4fe106f8715e40c464
stevenjchang <stevenjchang@gmail.com>
2020-12-14 16:40:40 -0800

    text: update hero text

--- src/components/Listing/ListingSearchBar.js
+++ src/components/Listing/ListingSearchBar.js
@@ -11,8 +11,8 @@ const ListingSearchBar = ({ inputValue, handleInputChange, handleSubmit }) => {
       <div className="bg-gray-100">
         <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between h-half">
           <h2 className="leading-none font-extrabold text-gray-900 text-2xl sm:text-4xl lg:text-5xl capitalize">
-            Trying to find <br />
-            International Roommates?
+            Find roommates! <br />
+            Internationally
             <p className="mx-auto max-w-screen-sm leading-tight text-base md:text-xl lg:text-3xl leading-7 text-indigo-600 mt-6">
               Use our custom filters and alerts to simplify your search.
             </p>
