b97ea65df7b4601bd2fd64bc7d35b7b0765c8c2b
stevenjchang <stevenjchang@gmail.com>
2020-12-23 18:57:37 -0800

    style: 1 radio button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,18 +4,8 @@ function SearchBar() {
   return (
     <>
       <div className="flex justify-center">
-        <form
-          action=""
-          className="flex justify-center border border-orange-500 p-1"
-        >
-          <input type="radio" className="tw-radio-input" value="true" />
-          <div className="tw-radio-card ">
-            <p className="">Private Room only</p>
-          </div>
-          <input type="radio" className="tw-radio-input" value="false" />
-          <div className="tw-radio-card ">
-            <p className="">Shared Room only</p>
-          </div>
+        <form action="" className="flex justify-center">
+          {" "}
           <input
             type="radio"
             className="tw-radio-input"
@@ -24,6 +14,14 @@ function SearchBar() {
           />
           <div className="tw-radio-card ">
             <p className="">Both</p>
+          </div>
+          <input type="radio" className="tw-radio-input" value="true" />
+          <div className="tw-radio-card ">
+            <p className="">Private Room only</p>
+          </div>
+          <input type="radio" className="tw-radio-input" value="false" />
+          <div className="tw-radio-card ">
+            <p className="">Shared Room only</p>
           </div>
         </form>
       </div>
